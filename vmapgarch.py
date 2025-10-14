6cd99b192de8cd0c23307b5bdb6938fb37caf8d9
GAVGAVGAVICH <alex.andr05@mail.ru>
2025-10-14 21:00:33 +0300

    TODO

--- vmapgarch.py
+++ vmapgarch.py
@@ -13,9 +13,25 @@ from arch import arch_model
 
 jax.config.update("jax_enable_x64", True)
 
+#Учитывая отсутствующие котировки опционов, получившееся распределение можно и в поверхность переводить
+
+# TODO 14/10/2025
+# 1. Variance targeting - DONE 
+# 2. Provide rescaling (??) - DONE 
+# 3. correct initial parameters initialization - DONE 
+# 4. Check out arch packages for their methods
+
 # TODO
-# 1. Variance targeting
-# 2. Provide rescaling (??)
+# 1. Non-Gaussian MLE estimation + Quasi MLE (consult arch/base/distributions.py)
+# 2. Introduce proper boundary values - w/o jnp.log etc. (together with persistence)
+# 3. Consider NOT estimating mean parameter
+# 4. persistence checks (PRIORYTY) - consider: kappa = 1 - alpha - beta 
+# 5. Test MLE w/ & w/o bounds. (naive)
+# 6. penalty MLE
+# 7. !!! GARCH quality metric (PRIORITY) 
+# 8. Design test for rescaled returns against non-resclaed
+# 9. Grid search for init parameters
+
 
 def nll(returns, parameters): 
     '''GARCH(1,1) negative Likelyhood'''
@@ -43,15 +59,19 @@ def nll(returns, parameters):
 @Auxiliary.timer
 @jax.jit
 def ManMLE_garch(returns, parameters):
+    '''Manual Maximum Likelyhood Estimation for the GARCH parameters. 
+    The initial guess (parameters before estimation) have to be prespecified
+    Here logarithm transformation is applied for alpha, beta and omega parameters (for bounding on [0;+inf])'''
     alpha, beta, omega, mu = parameters 
 
-    #ensuring non-neegativity of the variance parameres
+    #ensuring non-negativity of the variance parameres
     variance_greeks = jnp.array([alpha, beta, omega])
     log_alpha, log_beta, log_omega = jnp.log(variance_greeks)
 
     opt_params = jnp.array([log_alpha, log_beta, log_omega, mu])
 
-    def objective_function(optimization_parameters, returns): 
+    def objective_function(optimization_parameters, returns):
+        '''input parameters are transformed into their original version'''
         log_alpha, log_beta, log_omega, mu = optimization_parameters
         log_variance_greeks = jnp.array([log_alpha, log_beta, log_omega])
         alpha, beta, omega = jnp.exp(log_variance_greeks)
@@ -61,6 +81,92 @@ def ManMLE_garch(returns, parameters):
     result = minimize(objective_function, opt_params, args=(returns, ), method='BFGS')
 
     return result 
+
+@jax.jit 
+def ManMLE_garch_naive(returns, parameters): 
+    '''Errors may occure in optimization as parameters: jax.Array are optimized as well as input.'''
+    def objective_function(optimization_parameters, returns): 
+        return nll(returns, optimization_parameters)
+        
+    result = minimize(objective_function, parameters, args=(returns, ), method='BFGS')
+
+    return result 
+
+@Auxiliary.timer
+@jax.jit
+def VTE_garch(returns, parameters): 
+    '''Variance Targerting Estimation of GARCH parameters. 
+    For details on the method consult https://mpra.ub.uni-muenchen.de/15143/1/MPRA_paper_15143.pdf. 
+    The Script presented here is the JAX realization of the method described in the paper. 
+    Here logarithm transformation is applied for alpha and beta parameters (for bounding on [0;+inf]) 
+    !!! TRY logarithm bounding kappa (1- alpha - beta)
+    !!! Handle the persistence condition 
+    '''
+    #checks for persistence are necessary
+    alpha, beta, mu = parameters 
+    residuals = (returns - jnp.mean(returns))**2 
+    gamma = jnp.var(residuals) # one can move these calculations into objevtive_function()
+    # omega = gamma * kappa '
+    
+    #ensuring non-negativity of the variance parameres
+    variance_greeks = jnp.array([alpha, beta])
+    log_alpha, log_beta= jnp.log(variance_greeks) 
+
+    opt_params = jnp.array([log_alpha, log_beta, mu])
+
+    def objective_function(optimization_parameters, returns, gamma):
+        '''input parameters are transformed into their original version'''
+        log_alpha, log_beta, mu = optimization_parameters
+        log_variance_greeks = jnp.array([log_alpha, log_beta])
+        alpha, beta = jnp.exp(log_variance_greeks)
+        
+        #omega handling 
+        kappa = 1 - alpha - beta 
+        omega = gamma * kappa 
+
+        ll_parameters = jnp.array([alpha, beta, omega, mu])
+        return nll(returns, ll_parameters)
+
+    result = minimize(objective_function, opt_params, args=(returns, gamma), method='BFGS')
+
+    return result 
+
+def VTE_garch_naive(returns, parameters):
+    
+    def objective_function(optimization_parameters, returns): 
+        alpha, beta, mu = optimization_parameters
+
+        residuals = (returns - jnp.mean(returns))**2 
+        gamma = jnp.var(residuals) # one can move these calculations into objevtive_function()
+        kappa = 1 - alpha - beta
+        omega = gamma * kappa 
+
+        ll_parameters = jnp.array([alpha, beta, omega, mu])
+        return nll(returns, ll_parameters)
+    
+    result = minimize(objective_function, parameters, args=(returns, gamma), method='BFGS') 
+
+    return result   
+
+def initialize_parameters_test(returns): 
+    alpha = 0.1 
+    beta = 0.85 
+    mu = np.mean(returns)
+    
+    residuals = (returns - mu)**2 
+    gamma = np.var(residuals)
+
+    kappa = 1 - alpha - beta 
+    omega = kappa * gamma 
+
+    return {
+        'alpha': alpha,
+        'beta': beta,
+        'mu': mu,
+        'gamma': gamma, 
+        'kappa': kappa,
+        'omega': omega
+    }
 
 def simulation(mean, std, size): 
     _ = stats.norm.rvs(loc=mean, scale=std, size=size)
@@ -108,17 +214,37 @@ if __name__ == '__main__':
     beta = 0.9
     omega = jnp.var(returns) * (1 - alpha - beta)
 
-    parameters = jnp.array([alpha, beta, omega, mu])
+    parameters_MMLE = jnp.array([alpha, beta, omega, mu])
 
-    results = ManMLE_garch(returns, parameters)
+    results_MMLE = ManMLE_garch(returns, parameters_MMLE)
 
-    print(jnp.exp(results.x[:3]), results.x[4])
+    print(jnp.exp(results_MMLE.x[:3]), results_MMLE.x[4])
 
-    print(results.status)
+    print(results_MMLE.status)
 
-    # fit a GARCH(1,1) with constant mean
-    am = arch_model(returns, mean='Constant', vol='GARCH', p=1, q=1, dist='normal')
-    res = am.fit(disp='off')
+    print('+++++++++++++++++++++++++++++++++')
 
-    # print(res.summary())
-    print("params:", res.params)
+    returns_rescaled = 100 * returns 
+
+    results_MMLE_rescaled = ManMLE_garch(returns_rescaled, parameters_MMLE)
+
+    print(jnp.exp(results_MMLE_rescaled.x[:3]), results_MMLE_rescaled.x[4])
+
+    print(results_MMLE_rescaled.status)
+
+    print('+++++++++++++++++++++++++++++++++')
+
+    parameters_VTE = jnp.array([alpha, beta, mu])
+
+    results_VTE = VTE_garch(returns, parameters_VTE)
+
+    print(jnp.exp(results_VTE.x[:2]), results_VTE.x[3])
+
+    print(results_VTE.status)
+
+    # # fit a GARCH(1,1) with constant mean
+    # am = arch_model(returns, mean='Constant', vol='GARCH', p=1, q=1, dist='normal')
+    # res = am.fit(disp='off')
+
+    # # print(res.summary())
+    # print("params:", res.params)
