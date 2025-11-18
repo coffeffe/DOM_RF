d315b9b852a018662d45ebd83c8a0c6b52e59e64
GAVGAVGAVICH <alex.andr05@mail.ru>
2025-11-18 22:36:46 +0300

    Slight modification + GE pending

--- GARCH_VaR_delete_or_merge.py
+++ GARCH_VaR_delete_or_merge.py
@@ -121,3 +121,44 @@ def EGARCHo1_simulation(returns, horizon=10, n_paths=50_000, granularity=1, alph
     VaR = historical_var(sim_returns, alpha)
     
     return VaR
+
+
+from arch.univariate import GARCH, EWMAVariance, EGARCH, RiskMetrics2006
+from arch.univariate import Normal, StudentsT
+from arch.univariate import ARX, HARX, ARCHInMean, LS, ConstantMean
+
+__VOLATILITY__ = {'GARCH': GARCH, 'TARCH': GARCH, 'EWMAVariance': EWMAVariance, 'EGARCH': EGARCH}
+__DISTRIBUTIONS__ = {'norm': Normal, 't': StudentsT}
+__MEAN__ = {'arx': ARX, 'harx': HARX, 'LS': LS, 'a-i-m': ARCHInMean, 'CM': ConstantMean}
+
+def model_construction(mean_process: str, volatiltiy_process: str, dist_process: str, lags, **kwargs): 
+    def simulate(returns, horizon=10, n_paths=50_000, granularity=1, alpha=0.01): 
+        vol = __VOLATILITY__[volatiltiy_process](**kwargs)
+        if mean_process == 'CM' or mean_process == 'LS':
+            model = __MEAN__[mean_process](returns)
+        if mean_process == 'harx' or mean_process == 'a-i-m' or mean_process == 'arx':
+            model = __MEAN__[mean_process](returns, lags=lags)
+        model.volatility = vol 
+        model.distribution = __DISTRIBUTIONS__[dist_process]()
+        results = model.fit(disp='off')
+        h = int(horizon)                          
+        forecast = results.forecast(horizon=h, method='simulation')          
+        # mu = results.params.get("mu", 0) - used in the case of a single mean 
+        mu = forecast.mean.values[0]
+        sigma2 = forecast.variance.values[0]
+        sigma = sigma2 ** (1/2) #garch model estimates the variance of the underlyinh sample
+        parameters = SimParams(volatility=sigma, mean=mu)
+
+        if dist_process == 'norm': 
+            sim_returns = solve_local_vol_mean_gbm_log(parameters, 10, n_paths=n_paths)
+        
+        if dist_process == 't': 
+            nu = results.params.get('nu', 0)
+            simulator = make_random_path_simulator_local_vol_mean_student(parameters, 10, nu, granularity=granularity)
+            sim_returns = _terminal_returns(simulator=simulator, n_paths=n_paths) 
+
+        VaR = historical_var(sim_returns, alpha)
+        
+        return VaR
+
+    return simulate
